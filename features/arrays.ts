1e16e7e21426742c4703a390a3da8a5a6a9fe248
Chris <1203164317@qq.com>
2023-04-30 22:27:13 +0800

    day2 finish 029-043

---
+++ features/arrays.ts
@@ -0,0 +1,10 @@
+const carMakers: string[] = ["ford", "toyota", "chevy"];
+const dates = [new Date(), new Date()];
+const carByMaker = [["rfew"], ["Desa"], ["vgsd"]];
+
+const car = carMakers.pop();
+carMakers.map((car) => {
+  return car;
+});
+
+const importantDates: (string | Date)[] = [new Date(), "fsaf"];
---
+++ features/tuples.ts
@@ -0,0 +1,5 @@
+type Drink = [string, number];
+// Initialize it
+let x: Drink = ["hello", 10]; // OK
+let first = x[0];
+let sec = x[1];
